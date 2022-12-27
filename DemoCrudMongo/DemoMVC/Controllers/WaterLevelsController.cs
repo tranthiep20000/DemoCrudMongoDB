48084a237f0d3ceab223a22c20e7e4b98b23c0fe
tranthiep20000 <trantrungthiep13022000@gmail.com>
2022-12-27 21:43:38 +0700

    push code

---
+++ DemoCrudMongo/DemoMVC/Controllers/WaterLevelsController.cs
@@ -0,0 +1,124 @@
+﻿using DemoMVC.Models;
+using DemoMVC.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    /// <summary>
+    /// Information of WaterLevelsController
+    /// CreatedBy: ThiepTT(30/10/2022)
+    /// </summary>
+    public class WaterLevelsController : Controller
+    {
+        private readonly IWaterLevelRepository _waterLevelRepository;
+
+        public WaterLevelsController(IWaterLevelRepository waterLevelRepository)
+        {
+            _waterLevelRepository = waterLevelRepository;
+        }
+
+        public IActionResult Index(string? valueSearch)
+        {
+            IEnumerable<WaterLevel> waterLevels;
+
+            if (string.IsNullOrEmpty(valueSearch))
+            {
+                waterLevels = _waterLevelRepository.GetAll();
+            }
+            else
+            {
+                waterLevels = _waterLevelRepository.GetBySearchValue(valueSearch);
+            }
+
+            return View(waterLevels);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(WaterLevel waterLevel)
+        {
+            if (ModelState.IsValid)
+            {
+                waterLevel.Id = Guid.NewGuid();
+                _waterLevelRepository.Create(waterLevel);
+                TempData["success"] = "user created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //GET
+        public IActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var waterLevel = _waterLevelRepository.GetById((Guid)id);
+
+            if (waterLevel == null)
+            {
+                return NotFound();
+            }
+
+            return View(waterLevel);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Guid id, WaterLevel waterLevel)
+        {
+            if (ModelState.IsValid)
+            {
+                _waterLevelRepository.Update(id, waterLevel);
+                TempData["success"] = "user updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //GET
+        public IActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var waterLevel = _waterLevelRepository.GetById((Guid)id);
+
+            if (waterLevel == null)
+            {
+                return NotFound();
+            }
+
+            return View(waterLevel);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCategory(Guid id)
+        {
+            var waterLevel = _waterLevelRepository.GetById((Guid)id);
+
+            if (waterLevel == null)
+            {
+                return NotFound();
+            }
+
+            _waterLevelRepository.Delete((Guid)id);
+            TempData["success"] = "user deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DemoCrudMongo/DemoMVC/Models/WaterLevel.cs
@@ -0,0 +1,40 @@
+﻿using ServiceStack.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DemoMVC.Models
+{
+    /// <summary>
+    /// Information of WaterLevel
+    /// CreatedBy: ThiepTT(28/11/2022)
+    /// </summary>
+    public class WaterLevel
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// MeasurementDate
+        /// </summary>
+        [NotNull]
+        public DateTime MeasurementDate { get; set; }
+
+        /// <summary>
+        /// HeightWater
+        /// </summary>
+        [NotNull]
+        public double HeightWater { get; set; }
+
+        /// <summary>
+        /// CreatedDate
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// LastUpdatedDate
+        /// </summary>
+        public DateTime LastUpdatedDate { get; set; }
+    }
+}
